9c59dec34255d398f8b77592802120a93978013b
krazevedo <krazevedo@users.noreply.github.com>
2024-07-14 20:39:51 +0000

    deploy: ae5620e9397073c06871ddd227504830dda6fc7b

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"qj7VBAmJkfLr2e8Zloo/xByh2rabMKfVDMhSwdLQsgI=\"\n}"
--- src/utils/shorten.ts
+++
@@ -1,5 +0,0 @@
-export const shorten = (address: string): string => {
-	return `${address.substring(0, 6)}...${address.substring(
-		address.length - 4
-	)}`;
-};
